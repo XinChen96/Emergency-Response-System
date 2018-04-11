ceb591972ab52c68324bd46bd491db02195be50e
xinc <xinc@lafayette.edu>
2018-04-11 07:16:05 -0400

    Added initial parts of UI navigation for testing

--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,35 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private slots:
+
+    void on_login_clicked();
+
+    void on_menu_clicked();
+
+    void on_logoutEP_clicked();
+
+    void on_backMapEP_clicked();
+
+    void on_cancelReg_clicked();
+
+    void on_submitReg_clicked();
+
+    void on_reg_clicked();
+
+    void on_commEP_clicked();
+
+    void on_backComm_clicked();
+
+    void on_alerts_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_backRGroup_clicked();
+
+    void on_protocolEP_clicked();
+
+    void on_groupEP_clicked();
 
 private:
     Ui::MainWindow *ui;
