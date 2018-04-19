70037b7236eeb61536d4e84f3cbd16284420ea81
Ben Fuller <fullerb@lafayette.edu>
2018-04-19 12:17:21 -0400

    fixed merge...even more

--- controllers/controllers.pro
+++ controllers/controllers.pro
@@ -4,24 +4,12 @@ CONFIG -= app_bundle
 QT += sql
 
 SOURCES += main.cpp \
-<<<<<<< HEAD
     maincontroller.cpp \
     ../db/user_db.cpp \
     ../db/db_manager.cpp
-
-HEADERS += \
-    maincontroller.h \
-    ../db/user_db.h \
-    ../db/db_manager.h
-=======
-    maincontroller.cpp\
-    ../db/db_manager.cpp\
-    ../db/user_db.cpp
 
 
 HEADERS += \
     maincontroller.h\
     ../db/db_manager.h\
     ../db/user_db.h
-
->>>>>>> 3a18b025449d259385e369773772235841e5de4c
--- controllers/maincontroller.h
+++ controllers/maincontroller.h
@@ -1,14 +1,8 @@
 #ifndef MAINCONTROLLER_H
 #define MAINCONTROLLER_H
-<<<<<<< HEAD
 #include "QtSql/QSqlDatabase"
 #include "QtSql/QSqlQuery"
-#include "../db/db_manager.h"
-=======
-
->>>>>>> 3a18b025449d259385e369773772235841e5de4c
 #include "../db/user_db.h"
-#include "../db/db_manager.h"
 
 
 class MainController
@@ -17,16 +11,13 @@ public:
     MainController();
     ~MainController();
 
-<<<<<<< HEAD
     const QString db_location;
     bool add_user();
     //DB_Manager *db = nullptr;
-=======
     bool add_user(QString,QString,QString);
 private:
-    QString db_location;
+    //QString db_location;
     DB_Manager *db;
->>>>>>> 3a18b025449d259385e369773772235841e5de4c
 };
 
 #endif // MAINCONTROLLER_H
