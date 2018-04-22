19122bfef8f4693d8046da623c59437ba848798f
Ben Fuller <fullerb@lafayette.edu>
2018-04-22 14:57:59 -0400

    added database build file

---
+++ test_db.sql
@@ -0,0 +1,31 @@
+/* Build tables for DB */
+CREATE TABLE IF NOT EXISTS groups (id integer PRIMARY KEY, groupName text NOT NULL UNIQUE, date text);
+CREATE TABLE users (id integer PRIMARY KEY, firstName text NOT NULL, lastName text NOT NULL, username text NOT NULL UNIQUE, role integer NOT NULL);
+CREATE TABLE IF NOT EXISTS userGroups (id integer PRIMARY KEY, group_id integer NOT NULL, user_id integer NOT NULL, FOREIGN KEY(group_id) REFERENCES groups(id), FOREIGN KEY(user_id) REFERENCES users(id));
+CREATE TABLE simulations (id integer PRIMARY KEY, name text NOT NULL UNIQUE, lat real, lng real, radius real, num_civilians integer, trigger integer);
+
+/* Create civilians */
+INSERT INTO users (firstName, lastName, username, role) VALUES("Ben", "Fuller", "fullerb", 0);
+INSERT INTO users (firstName, lastName, username, role) VALUES("Joseph", "Teddick", "teddickj", 0);
+INSERT INTO users (firstName, lastName, username, role) VALUES("Chen", "Xin", "xinc", 0);
+INSERT INTO users (firstName, lastName, username, role) VALUES("Robert", "Jordan", "jordanr", 0);
+INSERT INTO users (firstName, lastName, username, role) VALUES("Paul", "Erdos", "erdosp", 0);
+
+/* Create responders */
+INSERT INTO users (firstName, lastName, username, role) VALUES("Ben", "Fuller", "benresponder", 1);
+INSERT INTO users (firstName, lastName, username, role) VALUES("Joseph", "Teddick", "joeresponder", 1);
+INSERT INTO users (firstName, lastName, username, role) VALUES("Chen", "Xin", "chenresponder", 1);
+INSERT INTO users (firstName, lastName, username, role) VALUES("Robert", "Jordan", "robertresponder", 1);
+INSERT INTO users (firstName, lastName, username, role) VALUES("Paul", "Erdos", "paulresponder", 1);
+
+/* Create groups for responders */
+INSERT INTO groups (groupName, date) VALUES("Police Group 1", DATE('now'));
+INSERT INTO groups (groupName, date) VALUES("Police Group 2", DATE('now'));
+INSERT INTO groups (groupName, date) VALUES("Firemen Group 1", DATE('now'));
+
+/* Add responders to groups */
+INSERT INTO userGroups (group_id, user_id) VALUES(0,0); /* Add ben to police group 1 */
+INSERT INTO userGroups (group_id, user_id) VALUES(0,2); /* Add ben to firemen group 1 */
+INSERT INTO userGroups (group_id, user_id) VALUES(1,1); /* Add Joseph to police group 2 */
+INSERT INTO userGroups (group_id, user_id) VALUES(2,2); /* Add chen to firemen group 1 */
+INSERT INTO userGroups (group_id, user_id) VALUES(4,1); /* Add paul to police group 1 */
